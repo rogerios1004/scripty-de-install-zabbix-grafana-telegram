13586766289b375ec47321cc5fff2b7367bbf1c9
rogerios1004 <rogeriosardinha3@gmail.com>
2018-11-18 16:03:38 -0200

    Update telegram-notify.sh

--- telegram-notify.sh
+++ telegram-notify.sh
@@ -1,20 +1,27 @@
 #! /bin/bash
 #Autor: Adail Horst (the.spaww@gmail.com)
 #Site: http://www.spinola.net.br 
-### Script de integracao com o Telegram - Consulta mensagens ### 
+### Script de integracao com o Telegram ### 
 #set -x
 ENTER="
 ";
 USERID="$1";
+
 ARQUIVO="/usr/lib/zabbix/alertscripts/botinfo.txt";
 if [ -f "$ARQUIVO" ]; then
   KEY=$(cat $ARQUIVO);
 else
   KEY="CHAVE_DO_MEU_BOT";
 fi
-echo $ENTER;
-URL="https://api.telegram.org/bot$KEY/getUpdates"
 
-curl -s $URL | grep message | awk -F':' '{print $5","$6","$7","$8}' | sed 's/"//g' | awk -F',' '{print $1","$3" "$5",@"$7}' | sed 's/}//g'
+TIMEOUT="5";
+TEXT=$(echo -e "*$2* $ENTER$3" |  sed 's/PROBLEM/INCIDENTE/g');
 
-echo $ENTER;
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+
+echo "DATA - TIPO - USERID - TIMEOUT - URL" >> /tmp/telegram-notify.log
+echo "$(date +%s) - REQUEST - $USERID $TIMEOUT $URL " >> /tmp/telegram-notify.log
+RESPONSE=`curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&parse_mode=markdown&text=$TEXT" $URL`;
+echo "$(date +%s) - RESPONSE - $RESPONSE" >> /tmp/telegram-notify.log
+
+chown zabbix: /tmp/telegram-notify.log
